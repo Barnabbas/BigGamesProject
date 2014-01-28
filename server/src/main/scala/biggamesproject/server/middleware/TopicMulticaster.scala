3d66f18d49a91cfeefe5d24d68efbd93e36577bf
Stef van Schuylenburg <s.b.v.schuylenburg@student.tue.nl>
2014-01-28 17:16:02 +0100

    implementation of sending the topics
    SImple implementation using multicast
    Probably will change in the future

---
+++ server/src/main/scala/biggamesproject/server/middleware/TopicMulticaster.scala
@@ -0,0 +1,15 @@
+package biggamesproject.server.middleware
+
+import scala.actors.ActorRef
+
+
+/**
+ * A TopicPublisher which simply multicasts a number of topics to the clients
+ * @param client the Actors to multicast to
+ * @todo maybe create separated client class
+ */
+class TopicMulticaster(clients: Seq[ActorRef]) extends TopicPublisher {
+	
+	override def publish(topic: Topic, message: Any) = clients foreach (_ ! (topic.id, message))
+	
+}
